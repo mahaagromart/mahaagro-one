7255f4e2b40cf4ab643e9617fb8b232019dfcf60
Himanshu Vishwakarma <himanshuvishwakarma383@gmail.com>
2025-04-23 17:20:20 +0530

    worked

--- next.config.js
+++ next.config.js
@@ -1,37 +1,37 @@
 //  next.config.js
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "api.mahaagro.org",
-//         pathname: "/Content/**",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        // protocol: "http",
-        // hostname: "localhost",
-        // pathname: "/Content/**",
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5136',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "api.mahaagro.org",
+        pathname: "/Content/**",
       },
     ],
   },
 };
 
- module.exports = nextConfig;
+module.exports = nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         // protocol: "http",
+//         // hostname: "localhost",
+//         // pathname: "/Content/**",
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         port: '5136',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// };
+
+//  module.exports = nextConfig;
